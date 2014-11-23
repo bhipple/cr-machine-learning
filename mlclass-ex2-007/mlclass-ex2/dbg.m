58f19e05fb41fae1ee6856672da7aea426086782
bhipple <bhipple@bloomberg.net>
2014-11-22 21:43:01 -0500

    Finished prediction function

--- mlclass-ex2-007/mlclass-ex2/dbg.m
+++ mlclass-ex2-007/mlclass-ex2/dbg.m
@@ -10,3 +10,9 @@ initial_theta = zeros(n + 1, 1);
 theta = initial_theta;
 
 [cost, grad] = costFunction(initial_theta, X, y);
+
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+[theta, cost] = ...
+	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+p = predict(theta, X)
