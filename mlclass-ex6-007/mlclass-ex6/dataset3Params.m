e880be261e16930cdbf04d7ad9fa3d05ec35624f
bhipple <bhipple@bloomberg.net>
2014-12-07 09:10:21 -0500

    finished C, sigma parameter optimization

--- mlclass-ex6-007/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-007/mlclass-ex6/dataset3Params.m
@@ -23,10 +23,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+lowestError = inf;
+trials = [ 0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30 ];
 
+for i = 1:length(trials)
+    trialC = trials(i);
+    for j = 1:length(trials)
+        trialSigma = trials(j);
 
+        model = svmTrain(X, y, trialC, @(x1, x2) gaussianKernel(x1, x2, trialSigma));
+        predictions = svmPredict(model, Xval);
 
-
+        predError = mean(double(predictions ~= yval));
+        if(predError < lowestError)
+            lowestError = predError;
+            C = trialC;
+            sigma = trialSigma;
+        end
+    end
+end
 
 
 % =========================================================================
